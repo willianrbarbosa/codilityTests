9039cf83dce230ed977fd4b70e88e7002b09b02e
Willian Barbosa <williampgm@gmail.com>
2022-02-25 18:08:30 -0300

    First unique number

--- smallesMissingInteger.js
+++ smallesMissingInteger.js
@@ -14,7 +14,7 @@ Given A = [−1, −3], the function should return 1.
 Write an efficient algorithm for the following assumptions:
 
 N is an integer within the range [1..100,000];
-each element of array A is an integer within the range [−1,000,000..1,000,000].*/
+each element of array A is an integer within the range [-1,000,000..1,000,000].*/
 
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
